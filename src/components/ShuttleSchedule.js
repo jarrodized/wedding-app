6597118891d7eab000d6137b79fb230cd83babb4
jarrodized <jarrodized@yahoo.com>
2018-12-08 16:59:48 -0500

    fix times

--- src/components/ShuttleSchedule.js
+++ src/components/ShuttleSchedule.js
@@ -18,7 +18,7 @@ const ShuttleSchedule = () => (
             <h3>AMERICINN LODGE & SUITES</h3>
             <p>2:50 PM</p>
             <p>3:30 PM</p>
-            <p>4:00 PM</p>
+            <p>4:10 PM</p>
             <h3>CHARLEVOIX INN & SUITES</h3>
             <p>3:00 PM</p>
             <p>4:00 PM</p>
@@ -34,7 +34,7 @@ const ShuttleSchedule = () => (
             <p>11:10 PM</p>
             <h3>ONLY AMERICINN LODGE & SUITES</h3>
             <p>11:45 PM</p>
-            <p>12:20 PM</p>
+            <p>12:20 AM</p>
           </div>
         </div>
         <p>PLEASE ARRIVE AT LEAST FIVE MINUTES BEFORE DEPARTURE TIMES</p>
